90bcea13bd5615fca6ac7aa475c85932045bad35
indahkasturi <indahkasturiii@gmail.com>
2025-02-27 16:57:24 +0700

    Test PT.DIKA

---
+++ login.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+    const errorMessage = document.getElementById("errorMessage");
+
+    loginForm.addEventListener("submit", async function (event) {
+        event.preventDefault(); 
+
+   
+        const username = document.getElementById("username").value.trim();
+        const password = document.getElementById("password").value.trim();
+
+        if (!username || !password) {
+            errorMessage.textContent = "Username and password required!";
+            return;
+        }
+
+        try {
+            const response = await fetch("/api/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password }),
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                alert("Login success!");
+                window.location.href = "dashboard.html";
+            } else {
+                errorMessage.textContent = result.message;
+            }
+        } catch (error) {
+            errorMessage.textContent = "Internal Server Error";
+        }
+    });
+});
